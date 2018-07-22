91536c832fbfd2354dc8d58ce1e222202b7aa664
crassSandwich <christopherjanidlo@gmail.com>
2018-07-21 23:54:39 -0400

    Player movement

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMove : MonoBehaviour {
+
+	public float Speed;
+
+	CharacterController cc;
+
+	void Start () {
+		cc = GetComponent<CharacterController>();
+	}
+	
+	void Update () {
+		var velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Speed;
+		cc.SimpleMove(transform.TransformDirection(velocity));
+	}
+
+}
