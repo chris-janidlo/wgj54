093e5c3375c43511e3fb098e1f45f66a4e50c799
Chris Janidlo <christopherjanidlo@gmail.com>
2018-08-29 13:00:58 -0400

    Office rev one

---
+++ Assets/Scripts/_makeVisibleInPlayMode.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _makeVisibleInPlayMode : MonoBehaviour {
+
+	new public Renderer renderer;
+
+	void Start () {
+		renderer.enabled = true;
+	}
+}
