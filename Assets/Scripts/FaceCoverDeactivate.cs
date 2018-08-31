377512dee07a009db76fcc599fcf3fbc98021ed8
Chris Janidlo <christopherjanidlo@gmail.com>
2018-08-30 23:29:33 -0400

    Family portrait functionality

---
+++ Assets/Scripts/FaceCoverDeactivate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCoverDeactivate : MonoBehaviour {
+
+	public int MemosTillDeactivated;
+
+	int counter;
+
+	void Start () {
+		counter = MemosTillDeactivated;
+		Memo.MemoPickedUp += onMemoPickedUp;
+	}
+
+	void onMemoPickedUp (object o, System.EventArgs e) {
+		counter--;
+		if (counter <= 0) {
+			Destroy(gameObject);
+		}
+	}
+}
